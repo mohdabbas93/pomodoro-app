7e534ebef2ae2623de658f1f8c1ce2415799ad5d
Mohammad Abbas <mohdabbas@narbase.com>
2021-12-16 23:36:36 +0200

    Add a button to start a count down timer of two minutes and show the count down in the text view

--- app/src/main/java/com/mohdabbas/pomodoroapp/MainActivity.kt
+++ app/src/main/java/com/mohdabbas/pomodoroapp/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.mohdabbas.pomodoroapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupOnClickListeners()
+    }
+
+    private fun setupOnClickListeners() {
+        startButton.setOnClickListener {
+            setupCountDownTimer(120000) // Count down timer is set for 2 minutes just as a starting point
+        }
+    }
+
+    private fun setupCountDownTimer(durationInMillis: Long) {
+        object : CountDownTimer(durationInMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                updateCountDownText(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                Toast.makeText(applicationContext, "Timer is up", Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
+    private fun updateCountDownText(millisUntilFinished: Long) {
+        val minutes = (millisUntilFinished / 1000) / 60
+        val seconds = (millisUntilFinished / 1000) % 60
+
+        val timeLeftFormatted = String.format("%02d:%02d", minutes, seconds)
+
+        textView.text = timeLeftFormatted
     }
 }
