83db5ed23c23ff532f316a9a776c9a89301fc277
Mohammad Abbas <mohdabbas@narbase.com>
2022-01-25 15:50:41 +0200

    Add the functionality of starting and pausing the timer

--- app/src/main/java/com/mohdabbas/pomodoroapp/MainActivity.kt
+++ app/src/main/java/com/mohdabbas/pomodoroapp/MainActivity.kt
@@ -2,34 +2,43 @@ package com.mohdabbas.pomodoroapp
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val focusTimeInMillis = 120000L
+    private val countDownIntervalInMillis = 1000L
+    private var timeLeftInMillis = 0L
+
+    private lateinit var countDownTimer: CountDownTimer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        timeLeftInMillis = focusTimeInMillis
+
         setupOnClickListeners()
     }
 
+    private var isTimerRunning = false
     private fun setupOnClickListeners() {
-        startButton.setOnClickListener {
-            setupCountDownTimer(120000) // Count down timer is set for 2 minutes just as a starting point
+        toggoleTimerButton.setOnClickListener {
+            if (!isTimerRunning) {
+                startTimer()
+                toggoleTimerButton.changeText(R.string.pause)
+            } else {
+                pauseTimer()
+                toggoleTimerButton.changeText(R.string.start)
+            }
+            isTimerRunning = !isTimerRunning
         }
     }
 
-    private fun setupCountDownTimer(durationInMillis: Long) {
-        object : CountDownTimer(durationInMillis, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                updateCountDownText(millisUntilFinished)
-            }
-
-            override fun onFinish() {
-                Toast.makeText(applicationContext, "Timer is up", Toast.LENGTH_SHORT).show()
-            }
-        }.start()
+    private fun Button.changeText(resId: Int) {
+        text = getString(resId)
     }
 
     private fun updateCountDownText(millisUntilFinished: Long) {
@@ -39,5 +48,22 @@ class MainActivity : AppCompatActivity() {
         val timeLeftFormatted = String.format("%02d:%02d", minutes, seconds)
 
         textView.text = timeLeftFormatted
+    }
+
+    private fun startTimer() {
+        countDownTimer = object : CountDownTimer(timeLeftInMillis, countDownIntervalInMillis) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeftInMillis = millisUntilFinished
+                updateCountDownText(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                Toast.makeText(applicationContext, "Timer is up", Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
+    private fun pauseTimer() {
+        countDownTimer.cancel()
     }
 }
